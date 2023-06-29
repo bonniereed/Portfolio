b180619bcf0f835ef48b4c1f7bcf6ba82a64c2cc
bonniereed <bonniejeanreed@gmail.com>
2023-06-29 13:22:55 -0500

    added image changed some styling

--- src/app/components/pages/About.js
+++ src/app/components/pages/About.js
@@ -1,10 +1,10 @@
-
+import Image from 'next/image'
+import selfie from 'src/img/sketchselfie.png'
 function About() {
   return (
       <div>
           <h2 className="#">Bonnie Reed</h2>
-          <h3 className="#">About Me</h3>
-          <p>
+          <p><Image src={selfie}style={{width:'25em', height:'25em', borderRadius:'.25em', padding:'3em'}}alt="picture of Bonnie"></Image>
           Experienced Systems Administrator with a demonstrated history of scripting and automating solutions for Mac and Windows support and reporting.
 
 Created Brevity, an internal application for Support Engineers to easily access all tools in one place; Reducing Average Call Handle Time.
--- src/app/components/pages/Footer.js
+++ src/app/components/pages/Footer.js
@@ -9,14 +9,14 @@ import React from 'react';
 
 function Footer() {
     return(
-      <div style={{ position: 'absolute', left:0, right:0, bottom:0, height:'[footer-height]', display:'flex', justifyContent: 'center', marginBottom:'1em' }}><div>
+      <div style={{ margin:'.5em', position: 'absolute', left:0, right:0, bottom:0, height:'[footer-height]', display:'flex', justifyContent: 'center', marginBottom:'1em' }}><div>
           <div>
           
-          <a href="https://github.com/bonniereed" target="_blank"><Image src={github}width={50}height={50}alt="github icon" /></a>
-          <a href="https://www.linkedin.com/in/bonniejeanreed/" target="_blank"><Image src={linkedIn} width={50}height={50}alt="linkedin"/></a>
-          <a href="https://docs.google.com/document/d/125iDwRCvodsc1wb3oqWg6LH2N_irk4T0O9enKizgIEg/edit?usp=sharing" target="_blank"><Image class="resume" src={resume} width={50}height={50} alt="resume"/></a>
-          <a href="mailto:“bonniejeanreed@gmail.com" target="_blank"><Image src={email}width={50}height={50} alt="email"/></a>
-          <a href="sms:(+1)(469-418-1554)"><Image src={telephone} width={50}height={50}alt="sms"/></a>
+          <a style={{padding:'.15em'}} href="https://github.com/bonniereed" target="_blank"><Image src={github}width={50}height={50}alt="github icon" /></a>
+          <a style={{padding:'.15em'}} href="https://www.linkedin.com/in/bonniejeanreed/" target="_blank"><Image src={linkedIn} width={50}height={50}alt="linkedin"/></a>
+          <a style={{padding:'.15em'}} href="https://docs.google.com/document/d/125iDwRCvodsc1wb3oqWg6LH2N_irk4T0O9enKizgIEg/edit?usp=sharing" target="_blank"><Image class="resume" src={resume} width={50}height={50} alt="resume"/></a>
+          <a style={{padding:'.15em'}} href="mailto:“bonniejeanreed@gmail.com" target="_blank"><Image src={email}width={50}height={50} alt="email"/></a>
+          <a style={{padding:'.15em'}} href="sms:(+1)(469-418-1554)"><Image src={telephone} width={50}height={50}alt="sms"/></a>
           <div style={{display:'flex', justifyContent: 'center'}}>Copyright Spudworx LLC</div>
           </div>
       
