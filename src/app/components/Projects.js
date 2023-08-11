2ef52f5c38953aa0b8f021bc8dc24509d85c2d7a
bonniereed <bonniejeanreed@gmail.com>
2023-08-11 14:17:49 -0500

    changing card style

--- src/app/components/Projects.js
+++ src/app/components/Projects.js
@@ -1,17 +1,49 @@
-import marketplace from 'src/img/marketplace.png'
-import mordle from 'src/img/mordle.jpeg'
-import Image from 'next/image'
-import Link from 'next/link'
-function Projects() {
-    return (<>
-        <ul style={{listStyleType:'none', margin:'5em', padding:'5em', display:'flex', justifyContent:'space-around', margin:0, padding:0}}>
-        <li  style={{color:'inherit', textDecoration:'none', padding:'5em'}}>
-        <Link href="https://github.com/bonniereed/MarketplaceExtension/tree/main" rel="noopener noreferrer" target="_blank"><Image src={marketplace} width={200}height={200}alt="Marketplace chrome extension" /></Link></li>
-        <li style={{color:'inherit', textDecoration:'none', padding:'5em'}}><Link href="https://github.com/bonniereed/Mordle" rel="noopener noreferrer" target="_blank"><Image src={mordle} width={200}height={200}alt="Wordle Clone" /></Link></li>
-    </ul>
-     </>
-      
-    );
-  }
+import React, { useState } from 'react';
+import {
+Card
+} from 'reactstrap';
+
+const items = [
+  {
+    src:  'src/img/marketplace.png',
+    altText: 'Marketplace Extension',
+    caption: 'Marketplace Extension',
+    description:'This project gives a user quick access to their listings',
+    repo:'https://github.com/bonniereed/MarketplaceExtension',
+    key: 1,
+  },
+  {
+    src: 'src/img/mordle.jpeg',
+    altText: 'Wordle Clone',
+    caption: 'Wordle Clone',
+    repo:'https://github.com/bonniereed/Mordle',
+    description: 'This project was a fun game to recreate with two different APIs',
+    key: 2,
+  },
+];
+
+function Projects(args) {
+
+  const cards = items.map((item) => {
+
+<Card
+  style={{
+    width: '18rem'
+  }}
+>
+  <img
+    key={item.src}
+  />
+  <CardBody>
+    <CardTitle tag="h5">
+    {item.caption}
+    </CardTitle>
+    <Button>
+      {item.repo}
+    </Button>
+  </CardBody>
+</Card>
+});
+}
   
   export default Projects;
